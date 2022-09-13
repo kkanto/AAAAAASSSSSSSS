d270fed900f0a87de8b1431f596b5685cdc7d4a9
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-13 11:05:41 +0200

    Create gradle project

---
+++ src/main/java/de/allianz/AAAAAASSSSSSSS/AaaaaassssssssApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.AAAAAASSSSSSSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaaaaassssssssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaaaaassssssssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/AAAAAASSSSSSSS/AaaaaassssssssApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.AAAAAASSSSSSSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaaaaassssssssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
